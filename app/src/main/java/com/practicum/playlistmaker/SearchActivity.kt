b4f556ff5883421f8e993335dc77471ca77d44f7
Aleksandr <avblago@yandex.ru>
2023-12-17 17:17:34 +0300

    Спринт 11 Добавил: - работу с сетью (Retrofit); - плейсхолдеры.

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -6,11 +6,19 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
@@ -20,6 +28,7 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private var valueFromET = EMPTY
+    private var searchText = EMPTY
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(SEARCH_TEXT_KEY, valueFromET)
@@ -31,6 +40,13 @@ class SearchActivity : AppCompatActivity() {
 
     }
 
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://itunes.apple.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(ITunesApi::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find)
@@ -39,25 +55,43 @@ class SearchActivity : AppCompatActivity() {
         val clearButton = findViewById<ImageView>(R.id.clearButton)
         val editText = findViewById<EditText>(R.id.findEditText)
         val backButton = findViewById<ImageView>(R.id.backImage)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val notFoundPlaceholder = findViewById<LinearLayout>(R.id.notFoundPlaceholder)
+        val errorPlaceholder = findViewById<LinearLayout>(R.id.errorPlaceholder)
+        val updateButton = findViewById<Button>(R.id.updateButton)
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         editText.setText(valueFromET)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         val playlist = MockObjects.getPlaylist()
+        val tracks = ArrayList<Track>()
 
-        val trackAdapter = TrackAdapter(playlist)
+        //val trackAdapter = TrackAdapter(playlist)
+        //recyclerView.adapter = trackAdapter
+
+        val trackAdapter = TrackAdapter()
+        trackAdapter.tracks = tracks
         recyclerView.adapter = trackAdapter
+
 
         clearButton.setOnClickListener {
             val inputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(editText.windowToken, 0)
             editText.setText(EMPTY)
+            recyclerView.visibility = View.GONE
+            notFoundPlaceholder.visibility = View.GONE
+            errorPlaceholder.visibility = View.GONE
         }
 
         val watcher = object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int): Unit {}
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ): Unit {
+            }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if (p0.isNullOrEmpty()) {
@@ -72,6 +106,100 @@ class SearchActivity : AppCompatActivity() {
         }
 
         editText.addTextChangedListener(watcher)
+
+        fun iTunesServiceSearch(text: String) {
+            iTunesService
+                .search(text)
+                .enqueue(object : Callback<TrackResponse> {
+                    override fun onResponse(
+                        call: Call<TrackResponse>,
+                        response: Response<TrackResponse>
+                    ) {
+                        if (response.code() == 200) {
+                            tracks.clear()
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                tracks.addAll(response.body()?.results!!)
+                                trackAdapter.notifyDataSetChanged()
+                            }
+                            if (tracks.isEmpty()) {
+                                //nothing found
+                                recyclerView.visibility = View.GONE
+                                errorPlaceholder.visibility = View.GONE
+                                notFoundPlaceholder.visibility = View.VISIBLE
+                            } else {
+                                // found
+                                notFoundPlaceholder.visibility = View.GONE
+                                errorPlaceholder.visibility = View.GONE
+                                recyclerView.visibility = View.VISIBLE
+                            }
+                        } else {
+                            // something wrong
+                            recyclerView.visibility = View.GONE
+                            notFoundPlaceholder.visibility = View.GONE
+                            errorPlaceholder.visibility = View.VISIBLE
+                        }
+                    }
+
+                    override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                        recyclerView.visibility = View.GONE
+                        notFoundPlaceholder.visibility = View.GONE
+                        errorPlaceholder.visibility = View.VISIBLE
+                    }
+                })
+        }
+
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (editText.text.isNotEmpty()) {
+                    searchText = editText.text.toString()
+                    iTunesServiceSearch(searchText)
+                    /*iTunesService
+                        .search(editText.text.toString())
+                        .enqueue(object : Callback<TrackResponse> {
+                            override fun onResponse(
+                                call: Call<TrackResponse>,
+                                response: Response<TrackResponse>
+                            ) {
+                                if (response.code() == 200) {
+                                    tracks.clear()
+                                    if (response.body()?.results?.isNotEmpty() == true) {
+                                        tracks.addAll(response.body()?.results!!)
+                                        trackAdapter.notifyDataSetChanged()
+                                    }
+                                    if (tracks.isEmpty()) {
+                                        //nothing found
+                                        recyclerView.visibility = View.GONE
+                                        errorPlaceholder.visibility = View.GONE
+                                        notFoundPlaceholder.visibility = View.VISIBLE
+                                    } else {
+                                        // found
+                                        notFoundPlaceholder.visibility = View.GONE
+                                        errorPlaceholder.visibility = View.GONE
+                                        recyclerView.visibility = View.VISIBLE
+                                    }
+                                } else {
+                                    // something wrong
+                                    recyclerView.visibility = View.GONE
+                                    notFoundPlaceholder.visibility = View.GONE
+                                    errorPlaceholder.visibility = View.VISIBLE
+                                }
+                            }
+
+                            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                                recyclerView.visibility = View.GONE
+                                notFoundPlaceholder.visibility = View.GONE
+                                errorPlaceholder.visibility = View.VISIBLE
+                            }
+                        })*/
+                }
+                true
+            }
+            false
+        }
+
+        updateButton.setOnClickListener {
+            iTunesServiceSearch(searchText)
+        }
 
         backButton.setOnClickListener {
             finish()
