3e90a301f17a0a3b7f7fe9296a65a934d6e9dca8
Aleksandr <avblago@yandex.ru>
2024-06-04 14:22:20 +0300

    ДЗ 17 спринт. На проверку.

--- app/src/main/java/com/practicum/playlistmaker/di/RepositoryModule.kt
+++ app/src/main/java/com/practicum/playlistmaker/di/RepositoryModule.kt
@@ -24,7 +24,7 @@ val repositoryModule = module {
     }
 
     factory<PlayerRepository> {
-        PlayerRepositoryImpl(get()) // ?? Look at pRI
+        PlayerRepositoryImpl(get())
     }
 
     factory<SettingsRepository> {
